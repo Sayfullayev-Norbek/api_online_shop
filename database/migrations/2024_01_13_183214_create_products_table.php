c6d4544013b194419f16473861d6cf550f2759f4
SayfullayevNorbek <sayfullayevnorbek@gmail.com>
2024-01-20 21:02:26 +0500

    Catigories Migration, Cantroller Seeder vs Api Yozildi

--- database/migrations/2024_01_13_183214_create_products_table.php
+++ database/migrations/2024_01_13_183214_create_products_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("category_id")->constrained();
+            $table->string('name');
+            $table->integer('price');
+            $table->text("description");
             $table->timestamps();
         });
     }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,29 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'name'=>[
+                'uz'=>"Stol",
+                "ru"=>"Cтoл"
+            ]
+        ]);
+        Category::create([
+            'name'=>[
+                'uz'=>"Divan",
+                "ru"=>"Диван"
+            ]
+        ]);
+        Category::create([
+            'name'=>[
+                'uz'=>"Yotoq",
+                "ru"=>"Кровать"
+            ]
+        ]);
+        Category::create([
+            'name'=>[
+                'uz'=>"Stul",
+                "ru"=>"Cтyл"
+            ]
+        ]);
     }
 }
